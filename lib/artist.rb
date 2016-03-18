c5656e6f61a6b8745112a71456f263a8e0e031c9
Herbert Kagumba <habukagumba@gmail.com>
2016-03-18 11:05:18 +0300

    feature (models) create basic models
    - create song model
    - create artist model
    - create genre model

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def self.create(artist)
+    Artist.new(artist).save
+  end
+end
+
